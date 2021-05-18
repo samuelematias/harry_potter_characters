21e76c03156031f564d8234987e6b3fcb6ea17ad
Samuel Matias <hi@samuelematias.com>
2021-05-18 09:44:44 -0300

    chore: improving the code base

---
+++ lib/app/app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../pages/pages.dart';
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    //Transparent statusBar
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gamespedia',
+      home: const HomePage(),
+      theme: ThemeData(
+        primaryIconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        accentIconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
