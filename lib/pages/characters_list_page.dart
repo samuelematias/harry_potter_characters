fde657cecbeb4f5163af781c50ea47a6d9c80f05
Samuel Matias <hi@samuelematias.com>
2021-05-20 12:04:50 -0300

    chore: update page

--- lib/pages/characters_list_page.dart
+++ lib/pages/characters_list_page.dart
@@ -1,16 +1,90 @@
 import 'package:flutter/material.dart';
+import '../controllers/controllers.dart';
+import '../models/models.dart';
 
-class CharactersListPage extends StatelessWidget {
+class CharactersListPage extends StatefulWidget {
   const CharactersListPage({Key key}) : super(key: key);
+
+  @override
+  _CharactersListPageState createState() => _CharactersListPageState();
+}
+
+class _CharactersListPageState extends State<CharactersListPage> {
+  final HpController _controller = HpController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.getCharacters();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: const <Widget>[Text('Harry Potter Characters List')],
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (_, __) => _Body(controller: _controller),
         ),
       ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({@required this.controller, Key key}) : super(key: key);
+  final HpController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isLoading = controller.value == HpState.loading;
+    final bool isError = controller.value == HpState.error;
+
+    if (isError) {
+      return Center(
+        child: ElevatedButton(
+          onPressed: () => controller.getCharacters(),
+          child: const Text('Try Again'),
+        ),
+      );
+    }
+
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 700),
+      transitionBuilder: (child, animation) => FadeTransition(
+        opacity: animation,
+        child: child,
+      ),
+      child: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : _List(controller: controller),
+    );
+  }
+}
+
+class _List extends StatelessWidget {
+  const _List({@required this.controller, Key key}) : super(key: key);
+  final HpController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<HpCharactersModel> characters = controller.characters;
+
+    return ListView.builder(
+      itemCount: characters.length,
+      itemBuilder: (BuildContext context, int index) {
+        final HpCharactersModel character = characters[index];
+        final String imageURLHttps =
+            controller.imageURLToHttps(urlHttp: character.image);
+        return ListTile(
+          leading: Image.network(imageURLHttps),
+          title: Text(character.name),
+          subtitle: Text(character.house),
+          onTap: () {},
+        );
+      },
     );
   }
 }
